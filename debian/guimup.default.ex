c4b0c75ab2498d23059454292ca534d04fa74428
Ronald van Engelen <rvengelen@hethooghuis.nl>
2012-08-26 12:52:20 +0200

    debianized using dh_make --single --copyright gpl --createorig

---
+++ debian/guimup.default.ex
@@ -0,0 +1,10 @@
+# Defaults for guimup initscript
+# sourced by /etc/init.d/guimup
+# installed at /etc/default/guimup by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(guimup):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="guimup" command="/usr/bin/guimup"
